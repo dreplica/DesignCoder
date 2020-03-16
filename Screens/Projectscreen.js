6136113c0264b5df93c169bfe33a5fc43a993055
dreplica <adejo.david@aiesec.net>
2020-03-16 03:47:09 +0100

    gesture working for single project element, on close and expand on touch is active

--- Screens/Projectscreen.js
+++ Screens/Projectscreen.js
@@ -1,77 +1,117 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import {PanResponder,Animated} from 'react-native'
+import { PanResponder, Animated } from 'react-native'
 
 import Project from '../components/project';
-// let panMove = 
-const Projects = ({navigation}) =>{
+
+const getNextFunc = (args) => {
+  const next = args + 1;
+  if (next > projects.length - 1) {
+    return 0;
+  }
+  return next;
+}
+
+const Projects = ({ navigation }) => {
+  const [index, setIndex] = useState(0)
   const [pan, setpan] = useState(new Animated.ValueXY())
   const [scale, setScale] = useState(new Animated.Value(0.9))
   const [transform, setTransform] = useState(new Animated.Value(44))
+  const [thirdscale, setThirdScale] = useState(new Animated.Value(0.8))
+  const [thirdtransform, setThirdTransform] = useState(new Animated.Value(-44))
   const [_panResponder, set_panResponder] = useState();
-    useEffect(() => {
-      set_panResponder(PanResponder.create({
-        onPanResponderGrant:()=>{
-          Animated.spring(scale,{
-            toValue:1
-          }).start()
-          Animated.spring(transform,{
-            toValue:44
-          }).start()
-        },
-        onStartShouldSetPanResponder:()=> true,
-        onMoveShouldSetPanResponder:()=>true,
-        onPanResponderMove:Animated.event([
-          null,{dx:pan.x,dy:pan.y}
-        ]),
-        onPanResponderRelease:(e)=>{
-          Animated.spring(pan,{
-          toValue:{x:0,y:0}
-        }).start()
-          Animated.spring(scale, {
-            toValue: 0.9
-          }).start()
-          Animated.spring(transform, {
-            toValue: 44
-          }).start()
+  useEffect(() => {
+    set_panResponder(PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        Animated.spring(scale, {toValue: 1}).start()
+        Animated.spring(transform, {toValue: 0}).start()
+        Animated.spring(thirdscale, {toValue: 0.9}).start()
+        Animated.spring(thirdtransform, {toValue: 44}).start()
+      },
+      onPanResponderMove: Animated.event([
+        null, { dx: pan.x, dy: pan.y }
+      ]),
+      onPanResponderRelease: (e) => {
+        const positionY = pan.y.__getValue();
+        if (positionY > 200) {
+          Animated.spring(pan, {toValue: { x: 0, y: 1000 }
+          }).start(() => {
+            pan.setValue({ x: 0, y: 0 })
+            scale.setValue(0.9)
+            transform.setValue(44)
+            thirdscale.setValue(0.8)
+            thirdtransform.setValue(-44)
+            setIndex(()=>getNextFunc(index))
+            return;
+          })
+        }
+        Animated.spring(pan, {toValue: { x: 0, y: 0 }}).start()
+        Animated.spring(scale, {toValue: 0.9}).start()
+        Animated.spring(transform, {toValue: 44}).start()
+        Animated.spring(thirdscale, { toValue: 0.8 }).start()
+        Animated.spring(thirdtransform, { toValue: -44 }).start()
       }
-      }))
-    }, [])
+    }))
+  }, [index,pan])
   return (
     <Container>
       {_panResponder && <>
-      <Animated.View 
-      style={{transform:[
-        {translateX:pan.x},
-        {translateY:pan.y}
-      ]}}
-        {..._panResponder.panHandlers}
-      >
-        <Project 
-        title={"Price Tag"} 
-        image={require('../assets/background5.jpg')}
-        author={"By adejo David"}
-          text={"I am really happy that i am happy that i want to be happy in tI am really happy that i am happy that i want to be happy in the happy that i am thank you"}
-        />
-      </Animated.View>
-      <Animated.View 
-      style={{
-        position:'absolute',
-        zIndex:-1,
-        width:'100%',
-        height:'100%',
-        justifyContent:'center',
-        alignItems:'center',
-        transform:[{scale:scale},{translateY:transform}]}}
-      
-      >
-        <Project 
-        title={"Price Tag"} 
-        image={require('../assets/background5.jpg')}
-        author={"By adejo David"}
-          text={"I am really happy that i am happy that i want to be happy in tI am really happy that i am happy that i want to be happy in the happy that i am thank you"}
-        />
-      </Animated.View>
+        <Animated.View
+          style={{
+            transform: [
+              { translateX: pan.x },
+              { translateY: pan.y }
+            ]
+          }}
+          {..._panResponder.panHandlers}
+        >
+          <Project
+            title={projects[index].title}
+            image={projects[index].image}
+            author={projects[index].author}
+            text={projects[index].text}
+                      />
+        </Animated.View>
+        <Animated.View
+          style={{
+            position: 'absolute',
+            zIndex: -1,
+            width: '100%',
+            height: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+            transform: [{ scale: scale }, { translateY: transform }]
+          }}
+
+        >
+          <Project
+            title={projects[getNextFunc(index)].title}
+            image={projects[getNextFunc(index)].image}
+            author={projects[getNextFunc(index)].author}
+            text={projects[getNextFunc(index)].text}
+          />
+        </Animated.View>
+        <Animated.View
+          style={{
+            position: 'absolute',
+            zIndex: -2,
+            width: '100%',
+            height: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+            transform: [{ scale:thirdscale}, { translateY: thirdtransform }]
+          }}
+
+        >
+          <Project
+            title={projects[getNextFunc(index + 1)].title}
+            image={projects[getNextFunc(index + 1)].image}
+            author={projects[getNextFunc(index + 1)].author}
+            text={projects[getNextFunc(index + 1)].text}
+          />
+        </Animated.View>
       </>
       }
     </Container>
@@ -87,4 +127,28 @@ const Container = styled.View`
 `
 const Text = styled.Text`
   /* color:lightblue */
-` 
+`
+
+const projects = [
+  {
+    title: "Price Tag",
+    image: require('../assets/background5.jpg'),
+    author: "By adejo David",
+    text: "I am really happy that i am happy that i want to be happy in tI am really happy that i am happy that i want to be happy in the happy that i am thank you",
+
+  },
+  {
+    title: "Home Tag",
+    image: require('../assets/background6.jpg'),
+    author: "By David Adejo",
+    text: "I am really happy that i am happy that i want to be happy in tI am really happy that i am happy that i want to be happy in the happy that i am thank you",
+
+  },
+  {
+    title: "Comic Tag",
+    image: require('../assets/background7.jpg'),
+    author: "By Odrago David",
+    text: "I am really happy that i am happy that i want to be happy in tI am really happy that i am happy that i want to be happy in the happy that i am thank you",
+
+  },
+]
--- components/project.js
+++ components/project.js
@@ -1,22 +1,73 @@
-import React from 'react';
+import React,{useState} from 'react';
 import styled from 'styled-components/native';
-
+import {Ionicons} from "@expo/vector-icons"
+import { TouchableOpacity, TouchableWithoutFeedback,Animated, Dimensions, StatusBar } from 'react-native'
 export default function Project(props) {
+  const [width, setWidth] = useState(Dimensions.get('window').width)
+  const [height, setHeight] = useState(Dimensions.get('window').height)
+  const [dimension, setDimension] = useState({
+    width:new Animated.Value(315),
+    height:new Animated.Value(400),
+    titleTop:new Animated.Value(20),
+  })
+
+  const Resize = ()=>{
+    Animated.spring(dimension.width,{toValue:width}).start()
+    Animated.spring(dimension.height,{toValue:height}).start()
+    Animated.spring(dimension.titleTop,{toValue:40}).start()
+    StatusBar.setHidden(true)
+  }
+  
+  const close = ()=>{
+    Animated.spring(dimension.width, { toValue: 315 }).start()
+    Animated.spring(dimension.height, { toValue: 400 }).start()
+    Animated.spring(dimension.titleTop, { toValue: 20 }).start()
+    StatusBar.setHidden(true)
+
+  }
+
     return (
-      <Container>
-        <Cover>
-          <Image source={props.image} />
-          <Title>{props.title}</Title>
-          <Author>{props.author}</Author>
-        </Cover>
-      <Text>{props.text}</Text>
-      </Container>
+      <TouchableWithoutFeedback onPress={Resize}>
+        <Animationcontainer
+        width={dimension.width}
+        height={dimension.height}
+        >
+          <Cover>
+            <Image source={props.image} />
+            <AnimatedTitle style={{top:dimension.titleTop}}>{props.title}</AnimatedTitle>
+            <Author>{props.author}</Author>
+          </Cover>
+        <Text>{props.text}</Text>
+          <TouchableOpacity
+          onPress={close}
+          style={{
+            position:'absolute',
+            top:35, 
+            right:25
+          }}
+          >
+            <Closing>
+              <Ionicons name="md-close" color="546bfb" size={32}/>
+            </Closing>
+          </TouchableOpacity>
+        </Animationcontainer>
+      </TouchableWithoutFeedback>
   );
 }
+
+const Closing = styled.View`
+    width:30px;
+    height:30px;
+    border-radius:15px;
+    justify-content:center;
+    align-items:center;
+    background-color:white;
+`
 
 const Text = styled.Text`
   font-size:17px;
   margin:20px;
+  padding:5px;
   line-height:24px;
   color:#3c4560;
 `
@@ -41,6 +92,7 @@ const Title = styled.Text`
   width:300px;
   color:white;
 `
+const AnimatedTitle = Animated.createAnimatedComponent(Title)
 
 const Image = styled.Image`
   position:absolute;
@@ -63,3 +115,5 @@ const Cover = styled.View`
   elevation:20px;
   border-radius:20px;
 `;
+
+const Animationcontainer = Animated.createAnimatedComponent(Container)
